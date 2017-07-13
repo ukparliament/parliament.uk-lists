3a2099a6c54820764414a3d513101c71e72d6543
Bryony Watson <watsonb@parliament.uk>
2017-07-13 16:21:43 +0100

    all tests passing

--- app/controllers/contact_points_controller.rb
+++ app/controllers/contact_points_controller.rb
@@ -2,16 +2,15 @@ class ContactPointsController < ApplicationController
   before_action :data_check, :build_request
 
   ROUTE_MAP = {
-    index: proc { |params| ParliamentHelper.parliament_request.constituencies(params[:constituency_id]).contact_point }
+    index: proc { ParliamentHelper.parliament_request.contact_points },
+    show:  proc { |params| ParliamentHelper.parliament_request.contact_points(params[:contact_point_id]) }
   }.freeze
 
-  # Renders a contact point given a constituency id.
-  # @controller_action_param :constituency_id [String] 8 character identifier that identifies constituency in graph database.
-  # @return [Grom::Node] object with type 'http://id.ukpds.org/schema/ConstituencyGroup' which has a contact point.
   def index
-    @constituency = RequestHelper.filter_response_data(
-    @request,
-    'http://id.ukpds.org/schema/ConstituencyGroup'
-    ).first
+    @contact_points = RequestHelper.filter_response_data(
+      @request,
+      'http://id.ukpds.org/schema/ContactPoint'
+    )
   end
+
 end
