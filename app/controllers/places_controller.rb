4f6479846c52b92c9f3ca0284a8a7670666c2285
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-08-29 16:27:16 +0100

    Update /place to show region and constituency count
    * Updated /place/:place_id view
    * Updated tests
    * Updated PlacesController

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,19 @@
 class PlacesController < ApplicationController
+  before_action :data_check, :build_request
 
   ROUTE_MAP = {
+    index:  proc { Parliament::Utils::Helpers::ParliamentHelper.parliament_request.region_index },
   }.freeze
 
+  # Renders a list of regions
+  # @return [Array] Grom::Nodes of type 'http://data.ordnancesurvey.co.uk/ontology/admingeo/EuropeanRegion'.
   def index
+    @places = Parliament::Utils::Helpers::RequestHelper.filter_response_data(
+      @request,
+      'http://data.ordnancesurvey.co.uk/ontology/admingeo/EuropeanRegion'
+    )
+
+    @places = @places.sort_by(:gss_code)
+
   end
 end
