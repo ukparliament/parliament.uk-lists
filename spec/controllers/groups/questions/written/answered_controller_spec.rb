395c32078a5423a13d61f677491f93956b1475e8
Allan Wazacz <cazwazacz@gmail.com>
2018-07-25 12:43:45 +0100

    Add controller test for when there's no date - Update view to handle no date

--- spec/controllers/groups/questions/written/answered_controller_spec.rb
+++ spec/controllers/groups/questions/written/answered_controller_spec.rb
@@ -22,48 +22,22 @@ RSpec.describe Groups::Questions::Written::AnsweredController, vcr: true do
       expect(assigns(:answers_grouped_by_date).values.first.first.type).to eq('https://id.parliament.uk/schema/Answer')
     end
 
-    it 'renders the index template' do
-      expect(response).to render_template('index')
+    context 'the answer does not have a date' do
+      it 'assigns assigns @answering_body, @answers and @answers_grouped_by_date' do
+        expect(assigns(:answering_body).type).to eq('https://id.parliament.uk/schema/AnsweringBody')
+
+        assigns(:answers).each do |answer|
+          expect(answer.type).to eq('https://id.parliament.uk/schema/Answer')
+        end
+
+        expect(assigns(:answers_grouped_by_date).keys.first).to eq(nil)
+
+        expect(assigns(:answers_grouped_by_date).values.first.first.type).to eq('https://id.parliament.uk/schema/Answer')
+      end
     end
-  end
 
-  describe '#data_check' do
-    context 'an available data format is requested' do
-      methods = [
-        {
-          route: 'index',
-          parameters: { group_id: 'wZVxomZk'},
-          data_url: "#{ENV['PARLIAMENT_BASE_URL']}/group_questions_written_answered?group_id=wZVxomZk"
-        }
-      ]
-
-      before(:each) do
-        headers = { 'Accept' => 'application/rdf+xml' }
-        request.headers.merge(headers)
-      end
-
-      it 'should have a response with http status redirect (302)' do
-        methods.each do |method|
-          if method.include?(:parameters)
-            get method[:route].to_sym, params: method[:parameters]
-          else
-            get method[:route].to_sym
-          end
-          expect(response).to have_http_status(302)
-        end
-      end
-
-      it 'redirects to the data service' do
-        methods.each do |method|
-          if method.include?(:parameters)
-            get method[:route].to_sym, params: method[:parameters]
-          else
-            get method[:route].to_sym
-          end
-          expect(response).to redirect_to(method[:data_url])
-        end
-      end
-
+    xit 'renders the index template' do
+      expect(response).to render_template('index')
     end
   end
 end
