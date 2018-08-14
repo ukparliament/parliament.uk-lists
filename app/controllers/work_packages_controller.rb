6dcd70d7cd8ae509144e7b336adec0cd355ce935
Peter Hunt <naseberry@users.noreply.github.com>
2018-08-14 13:42:13 +0100

    [UTT-30] Remove navigation--main (#179)

--- app/controllers/work_packages_controller.rb
+++ app/controllers/work_packages_controller.rb
@@ -1,5 +1,5 @@
 class WorkPackagesController < ApplicationController
-  before_action :data_check, :build_request, :disable_top_navigation
+  before_action :data_check, :build_request
 
   ROUTE_MAP = {
     index: proc { Parliament::Utils::Helpers::ParliamentHelper.parliament_request.work_package_index },
